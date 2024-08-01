f09aae9be4d56dff5e1521b3f41440aaaf838c24
RichardWeller <richardweller@gmx.de>
2024-08-01 22:08:19 +0200

    feat: Set up the initial project

---
+++ src/components/Widget.svelte
@@ -0,0 +1,52 @@
+<script>
+    //Place the wild Github Kanban things here
+</script>
+
+<div class="body">
+    <div class="content-area">
+        <div class="title">
+            <p>Currently</p>
+            <p>working on</p>
+            <div class="divider">
+                <svg height="5" width="120" xmlns="http://www.w3.org/2000/svg">
+                    <line x1="0" y1="0" x2="120" y2="0" style="stroke:#000000; stroke-width: 3" />
+                </svg>
+            </div>
+        </div>
+        <div class="main-content">
+            Coming Soon!
+        </div>
+    </div>
+</div>
+
+<style>
+    @import '/src/lib/fonts.css';
+
+    .body{
+        font-family: 'HelveticaNowDisplay', sans-serif;
+        font-weight: bold;
+        position: absolute;
+        background-color: rgb(255 255 255 / 40%);
+        backdrop-filter: blur(10px);
+        height: 384px;
+        width: 350px;
+        border-radius: 15px;
+        right: 50px;
+        bottom: 0;
+        right: 0;
+    }
+
+    .content-area{
+        justify-self: center;
+        width: 80%;
+        height: 80%;
+        margin: 10% 10%;
+    }
+
+    .body p{
+        height: 7px;
+    }
+
+    .main-content{
+    }
+</style>
