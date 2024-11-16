d4f28ed124deb661bf7eb08b105670ecb71dcf31
knht <43456808+knht@users.noreply.github.com>
2024-11-16 20:05:35 +0100

    config: Add custom viewport breakpoints for desktop-first support

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,8 +15,18 @@ export default {
                 bold: "700",
                 "extra-bold": "800",
                 black: "900"
-            }
-        }
+            },
+            screens: {
+                mobile: { max: "479px" },
+                // => @media (max-width: 479px) { ... }
+
+                tablet: { max: "767px" },
+                // => @media (max-width: 767px) { ... }
+
+                desktop: { min: "1200px" },
+                // => @media (min-width: 1200px) { ... }
+            },
+        },
     },
     plugins: []
 } satisfies Config;
